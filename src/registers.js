9fcae97f27c8f15c6e3beb7a11a56d7daa632c37
Anar <anar.sol.dev@gmail.com>
2022-11-03 15:20:26 +0100

    create Registers class

--- src/registers.js
+++ src/registers.js
@@ -6,13 +6,33 @@ class RAMException extends Error {
 }
 
 class Registers {
-    #NB_GENERAL_REGISTERS = 16;
+    static V0 = 0x0;
+    static V1 = 0x1;
+    static V2 = 0x2;
+    static V3 = 0x3;
+    static V4 = 0x4;
+    static V5 = 0x5;
+    static V6 = 0x6;
+    static V7 = 0x7;
+    static V8 = 0x8;
+    static V9 = 0x9;
+    static VA = 0xA;
+    static VB = 0xB;
+    static VC = 0xC;
+    static VD = 0xD;
+    static VE = 0xE;
+    static VF = 0xF;
+    static DELAY = 0x10;
+    static SOUND = 0x11;
+    static SP = 0x12;
+    static I = 0x13;
+    static PC = 0x14;
 
-    #generalRegisters;
-    #addressRegister;
-    #soundRegister;
-    #delayRegister;
-    #programCounter;
+    static #NB_8BIT_REGISTERS = 19;
+    static #NB_16BIT_REGISTERS = 2;
+
+    #registers8bit;
+    #registers16bit;
 
     constructor() {
         this.#initialize();
@@ -23,23 +43,26 @@ class Registers {
     }
 
     #initialize() {
-        this.#generalRegisters = new Uint8Array(this.#NB_GENERAL_REGISTERS);
+        this.#registers8bit = new Uint8Array(Registers.#NB_8BIT_REGISTERS);
+        this.#registers16bit = new Uint16Array(Registers.#NB_16BIT_REGISTERS);
     }
 
     write(register, value) {
-        this.#generalRegisters[register] = value;
+        if (register < Registers.#NB_8BIT_REGISTERS) this.#registers8bit[register] = value;
+        else this.#registers16bit[register - Registers.#NB_8BIT_REGISTERS] = value;
     }
 
     read(register) {
-        return this.#generalRegisters[register];
+        if (register < Registers.#NB_8BIT_REGISTERS) return this.#registers8bit[register];
+        return this.#registers16bit[register - Registers.#NB_8BIT_REGISTERS];
     }
 
-    writeRange(register, array) {
+    writeRange(register = Registers.V0, array) {
         for (const value of array) this.write(register++, value);
     }
 
     readRange(register, length) {
-        return this.#generalRegisters.subarray(register, register + length);
+        return this.#registers8bit.subarray(register, register + length);
     }
 }
 
--- test/registers.test.js
+++ test/registers.test.js
@@ -2,6 +2,7 @@ import { Registers } from "../src/registers";
 
 describe("Registers", () => {
     const BYTE_VALUE = 127;
+    const DOUBLE_BYTE_VALUE = 32767;
     const BYTE_ARRAY = new Uint8Array([64, 127, 255]);
 
     let registers;
@@ -10,123 +11,114 @@ describe("Registers", () => {
         registers = Registers.newRegisters();
     });
 
-    test("write and read general register 0x0", () => {
-        const register = 0x0;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register V0", () => {
+        registers.write(Registers.V0, BYTE_VALUE);
+        expect(registers.read(Registers.V0)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0x1", () => {
-        const register = 0x1;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register V1", () => {
+        registers.write(Registers.V1, BYTE_VALUE);
+        expect(registers.read(Registers.V1)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0x2", () => {
-        const register = 0x2;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register V2", () => {
+        registers.write(Registers.V2, BYTE_VALUE);
+        expect(registers.read(Registers.V2)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0x3", () => {
-        const register = 0x3;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register V3", () => {
+        registers.write(Registers.V3, BYTE_VALUE);
+        expect(registers.read(Registers.V3)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0x4", () => {
-        const register = 0x4;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register V4", () => {
+        registers.write(Registers.V4, BYTE_VALUE);
+        expect(registers.read(Registers.V4)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0x5", () => {
-        const register = 0x5;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register V5", () => {
+        registers.write(Registers.V5, BYTE_VALUE);
+        expect(registers.read(Registers.V5)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0x6", () => {
-        const register = 0x6;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register V6", () => {
+        registers.write(Registers.V6, BYTE_VALUE);
+        expect(registers.read(Registers.V6)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0x7", () => {
-        const register = 0x7;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register V7", () => {
+        registers.write(Registers.V7, BYTE_VALUE);
+        expect(registers.read(Registers.V7)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0x8", () => {
-        const register = 0x8;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register V8", () => {
+        registers.write(Registers.V8, BYTE_VALUE);
+        expect(registers.read(Registers.V8)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0x9", () => {
-        const register = 0x9;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register V9", () => {
+        registers.write(Registers.V9, BYTE_VALUE);
+        expect(registers.read(Registers.V9)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0xA", () => {
-        const register = 0xA;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register VA", () => {
+        registers.write(Registers.VA, BYTE_VALUE);
+        expect(registers.read(Registers.VA)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0xB", () => {
-        const register = 0xB;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register VB", () => {
+        registers.write(Registers.VB, BYTE_VALUE);
+        expect(registers.read(Registers.VB)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0xC", () => {
-        const register = 0xC;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register VC", () => {
+        registers.write(Registers.VC, BYTE_VALUE);
+        expect(registers.read(Registers.VC)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0xD", () => {
-        const register = 0xD;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register VD", () => {
+        registers.write(Registers.VD, BYTE_VALUE);
+        expect(registers.read(Registers.VD)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0xE", () => {
-        const register = 0xE;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register VE", () => {
+        registers.write(Registers.VE, BYTE_VALUE);
+        expect(registers.read(Registers.VE)).toBe(BYTE_VALUE);
     });
 
-    test("write and read general register 0xF", () => {
-        const register = 0xF;
-
-        registers.write(register, BYTE_VALUE);
-        expect(registers.read(register)).toBe(BYTE_VALUE);
+    test("write and read general register VF", () => {
+        registers.write(Registers.VF, BYTE_VALUE);
+        expect(registers.read(Registers.VF)).toBe(BYTE_VALUE);
     });
 
     test("write and read a range of general registers", () => {
-        const register = 0x0;
+        registers.writeRange(Registers.V0, BYTE_ARRAY);
+        expect(registers.readRange(Registers.V0, BYTE_ARRAY.length)).toEqual(BYTE_ARRAY);
+    });
 
-        registers.writeRange(register, BYTE_ARRAY);
-        expect(registers.readRange(register, BYTE_ARRAY.length)).toEqual(BYTE_ARRAY);
+    test("write and read delay register", () => {
+        registers.write(Registers.DELAY, BYTE_VALUE);
+        expect(registers.read(Registers.DELAY)).toBe(BYTE_VALUE);
+    });
+
+    test("write and read sound register", () => {
+        registers.write(Registers.SOUND, BYTE_VALUE);
+        expect(registers.read(Registers.SOUND)).toBe(BYTE_VALUE);
+    });
+
+    test("write and read stack pointer register", () => {
+        registers.write(Registers.SP, BYTE_VALUE);
+        expect(registers.read(Registers.SP)).toBe(BYTE_VALUE);
+    });
+
+    test("write and read address register", () => {
+        registers.write(Registers.I, DOUBLE_BYTE_VALUE);
+        expect(registers.read(Registers.I)).toBe(DOUBLE_BYTE_VALUE);
+    });
+
+    test("write and read program counter register", () => {
+        registers.write(Registers.PC, DOUBLE_BYTE_VALUE);
+        expect(registers.read(Registers.PC)).toBe(DOUBLE_BYTE_VALUE);
     });
 
 });
